bbd7146407f215aecfe0572c71778500cb56253e
Vishnu kumar <work.vishnukumar@gmail.com>
2025-01-09 02:47:17 +0530

    made responsive for mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,16 +12,18 @@ function Card(props) {
       whileDrag={{ scale: 0.9, zIndex: 999, cursor: "grab" }}
       dragElastic={0.1}
       dragTransition={{ bounceStiffness: 30, bounceDamping: 20 }}
-      className="relative pt-4 h-[300px] w-[250px] overflow-hidden rounded-3xl bg-white flex-shrink-0 drop-shadow-lg flex flex-col gap-4"
+      className="relative pt-4 h-[250px] w-[200px] overflow-hidden rounded-3xl bg-white flex-shrink-0 drop-shadow-lg flex flex-col gap-4 md:h-[300px] md:w-[250px]"
     >
       <div className="px-5">
         <FileText size={20} />
       </div>
       <div className="px-5 flex-1 flex-shrink-0 overflow-auto">
-        <p className="text-[13px] font-medium">{cardData?.description}</p>
+        <p className="text-xs font-medium md:text-[13px]">
+          {cardData?.description}
+        </p>
       </div>
       <div
-        className={`px-5 text-sm flex items-center gap-3 justify-between ${
+        className={`px-5 text-[13px] flex items-center gap-3 justify-between md:text-sm ${
           !cardData.close ? "pb-4" : ""
         }`}
       >
@@ -33,7 +35,9 @@ function Card(props) {
       {cardData.close && (
         <div>
           <button
-            className={`p-4 w-full text-sm font-medium text-white duration-200 hover:brightness-90`}
+            className={
+              "p-4 w-full text-[13px] font-medium text-white duration-200 hover:brightness-90 md:text-sm "
+            }
             style={{
               backgroundColor: cardData?.tag?.tagColor,
             }}
